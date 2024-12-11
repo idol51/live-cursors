aae5b5434ca87daa75fbe06192afbf230141edab
idol51 <armaanlodi.123@gmail.com>
2024-12-11 12:11:16 +0530

    Added dynamic backend url

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -2,5 +2,5 @@ import { io, Socket } from "socket.io-client";
 import { ClientToServerEvents, ServerToClientEvents } from "@shared/types";
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  "http://localhost:3000"
+  import.meta.env.VITE_BACKEND_URL
 );
